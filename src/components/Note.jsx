2beee6af30a7be3d9402cb9ccf17df4c0a791e96
Jibbyworks <JDBucog@yahoo.com>
2021-07-05 18:00:31 -0700

    initial commit

---
+++ src/components/Note.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Note() {
+
+  let noteTitle = ''
+  let noteContent = ''
+
+  return (
+    <div className="note">
+      <h2>{noteTitle = "This is the note title"}</h2>
+      <p>{noteContent = "This is the note content"}</p>
+    </div>
+  )
+}
+
+export default Note
